84911158fb0de3018905ca35750e48fedc3a8c7c
cscsenge13 <csiki.csenge@gmail.com>
2024-10-06 17:29:51 +0200

    Nyolcadik: navigáció+boritó

--- javascript.js
+++ javascript.js
@@ -1,10 +1,22 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
 
-  
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const currentLocation = window.location.pathname;
+  const navLinks = document.querySelectorAll(".topnav a");
+
+  navLinks.forEach(link => {
+      if (link.href === window.location.href) {
+          link.classList.add("active"); // Aktív osztály hozzáadása az aktuális linkhez
+      } else {
+          link.classList.remove("active"); // Eltávolítja az aktív osztályt más linkekről
+      }
+  });
+})
